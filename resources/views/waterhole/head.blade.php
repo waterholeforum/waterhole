03c8f5b0986ab298b37c4e26acb1d33de5a79c78
Toby Zerner <toby.zerner@gmail.com>
2023-03-27 08:21:46 +1030

    Add comment about favicon

--- resources/views/waterhole/head.blade.php
+++ resources/views/waterhole/head.blade.php
@@ -6,4 +6,7 @@
 | HTML added to this template will show up in the <head> tag on every page.
 | This is a great place to add any custom <link> and <meta> tags.
 |
+| If you're looking to add a favicon, this is a great resource:
+| https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
+|
 --}}
